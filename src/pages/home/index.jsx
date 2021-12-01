ab186a3f5ffb0f468ea0e947e1cf126c157db7dd
steph-crown <emmanuelstephen024@gmail.com>
2021-12-01 21:25:30 +0100

    completed banner section home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,11 +3,45 @@ import { Footer } from "../../components/footer";
 import { GetStarted } from "../../components/get-started";
 import { NavBar } from "../../components/nav";
 import { HomepageContainer } from "./style";
+import AppleStore from "./../../assets/icons/applestore.svg";
+import GooglePlay from "./../../assets/icons/googleplay.svg";
+import gradient from "./../../assets/images/gradient-bg.svg";
+import phoneAlone from "./../../assets/images/phone-alone.png";
+import phoneAndCode from "./../../assets/images/phone-and-code.png";
 
 export const HomePage = () => {
     return (
         <HomepageContainer>
-            <NavBar />
+            <section className="banner">
+                <img src={gradient} alt="" className="gradient" />
+                <NavBar />
+                <div className="banner">
+                    <div className="left">
+                        <h1>
+                            Deposit and withdraw Cash and{" "}
+                            <span className="circled">Crypto</span> assets with
+                            Hifast
+                        </h1>
+                        <p>
+                            Explore a world of boundless opportunities with
+                            Hifast and get unlimited access to a better
+                            financial experience designed to get you to where
+                            you need to be.
+                        </p>
+                        <div className="downloads">
+                            <a href="/">
+                                <img src={GooglePlay} alt="" />
+                            </a>{" "}
+                            <a href="/">
+                                <img src={AppleStore} alt="" />
+                            </a>
+                        </div>
+                    </div>
+                    <div className="sm-hide right">
+                        <img src={phoneAndCode} alt="" />
+                    </div>
+                </div>
+            </section>
             <GetStarted />
             <Faq />
             <Footer />
--- src/pages/home/style.jsx
+++ src/pages/home/style.jsx
@@ -1,3 +1,117 @@
 import styled from "styled-components";
+import { breakpoints } from "../../utils/breakpoints";
+import { colors } from "../../utils/colors";
+import { padding } from "../../utils/spacing";
 
-export const HomepageContainer = styled.div``;
+import Ellipse from "./../../assets/icons/ellipse.svg";
+
+export const HomepageContainer = styled.div`
+    section.banner {
+        position: relative;
+        img.gradient {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+        }
+
+        @media (max-width: ${breakpoints.sm}) {
+            img.gradient {
+                position: absolute;
+                top: -30px;
+                left: -80px;
+                width: 100%;
+            }
+        }
+        div.banner {
+            width: 100%;
+            position: relative;
+            display: flex;
+            justify-content: space-between;
+
+            div.right {
+                /* width: 50%; */
+                right: 0;
+                top: 0;
+
+                img {
+                    height: 640px;
+
+                    @media (max-width: 1200px) {
+                        height: 560px;
+                    }
+                }
+            }
+
+            h1 {
+                font-size: 3.6em;
+                color: black;
+                line-height: 72px;
+
+                span.circled {
+                    background-image: url(${Ellipse});
+                    background-size: cover;
+                    background-position: center;
+                    padding: 0 1px;
+                    display: inline-block;
+                    background-repeat: no-repeat;
+                }
+            }
+
+            p {
+                font-size: 18px;
+                margin-top: 1.5em;
+                line-height: 30px;
+            }
+
+            div.left {
+                width: 55%;
+                padding: 2em 0 2em ${padding.mainPadding.lg};
+                @media (max-width: ${breakpoints.sm}) {
+                    padding: 2.2em ${padding.mainPadding.small};
+                    display: block;
+                    .sm-hide {
+                        display: none;
+                    }
+                }
+            }
+
+            @media (max-width: 1024px) {
+                padding: 1.5em ${padding.mainPadding.small};
+                display: block;
+                div.left {
+                    width: 100%;
+                }
+                h1 {
+                    font-size: 2em;
+                    line-height: 40px;
+                }
+                .sm-hide {
+                    display: none;
+                }
+            }
+
+            div.downloads {
+                display: flex;
+                justify-content: flex-start;
+                margin-top: 20px;
+
+                & > a:first-child {
+                    margin-right: 1em;
+                }
+
+                @media (max-width: ${breakpoints.sm}) {
+                    margin-top: 14px;
+
+                    a img {
+                        height: 40px;
+                    }
+                }
+            }
+
+            a img {
+                height: 50px;
+            }
+        }
+    }
+`;
